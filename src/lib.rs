ca48d9aec1c789319b45ae9acc963bc461abfec6
Johan Burell <johan.burell@valtech.com>
2021-12-06 23:41:42 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,44 @@ pub fn calc_power_consumption(data: Vec<u32>, num_bits: usize) -> u32 {
   gamma * epsilon
 }
 
+#[derive(Debug, Clone)]
+struct Elem((u32, Vec<u32>), (u32, Vec<u32>));
+impl Elem {
+  pub fn new() -> Self {
+    Elem((0, Vec::new()), (0, Vec::new()))
+  }
+}
+
 pub fn calc_oxygen_and_co2(data: Vec<u32>, num_bits: usize) -> u32 {
-  let r: Vec<u32> = 
+  let r = 
     data.iter()
-    .fold(vec![((0, Vec::new()), 0, Vec::new()); num_bits], |((count_ones, acc_ones), (count_zeroes, acc_zero)), bits|{
-      acc.into_iter().enumerate()
-      .map(|(n, v)| {
-        v + ((bits >> n) & 0b1)
-      }).collect()
+    .fold(vec![Elem::new(); num_bits], |acc, bits|{
+      let res = 
+      acc
+      .iter()
+      .enumerate()
+      .map(|(n, elem)| {
+        let (count_zeroes, acc_zeroes) = elem.0;
+        let (count_ones, acc_ones) = elem.1;
+        match (bits >> n) & 0b1 {
+          0 => {
+            acc_zeroes.push(*bits);
+            Elem((count_zeroes + 1, acc_zeroes), (count_ones, acc_ones))
+          }
+          1 => {
+            acc_ones.push(*bits);
+            Elem((count_zeroes, acc_zeroes), (count_ones + 1, acc_ones))
+          }
+          _ => panic!("only ones and zeroes possible")
+        }
+      })
+      .collect();
+
+      res
+    })
+    .fold(Vec::new(), |acc, vec| {
+      acc.push(vec);
+      acc
     });
 
   let num_words_div_2  = data.len() as u32 >> 1;
